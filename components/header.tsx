580abc75a4953fbd8aa36f2c6688aa5bacc11b48
Arceister <imandajagad01@gmail.com>
2023-07-01 07:42:20 +0700

    feat: add `header` component

---
+++ components/header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FC, useState } from "react";
+
+const Header: FC = () => {
+  //TODO: Change local useState to a global state (Context)
+  const [activeIndicator, setActiveIndicator] = useState(0)
+
+  const activeCondition = (child: number) => activeIndicator === child ? "font-extrabold" : "font-semibold"
+
+  return (
+    <nav className="flex justify-center lg:justify-between flex-wrap sm:p-3 lg:p-5">
+      <div className="w-full block">
+        <div className="flex justify-around sm:text-xl lg:text-2xl">
+          <Link 
+            href='/' 
+            className={`block mt-4 lg:inline-block lg:mt-0 text-slate-800 hover:text-black mr-4 ${activeCondition(0)}`} 
+            onClick={() => {setActiveIndicator(0)}}>
+            Home
+          </Link>
+          <Link href='/' 
+            className={`block mt-4 lg:inline-block lg:mt-0 text-slate-800 hover:text-black mr-4 ${activeCondition(1)}`}
+            onClick={() => {setActiveIndicator(1)}}>
+            Blog
+          </Link>
+          <Link href='/' 
+            className={`block mt-4 lg:inline-block lg:mt-0 text-slate-800 hover:text-black mr-4 ${activeCondition(2)}`}
+            onClick={() => {setActiveIndicator(2)}}>
+            Projects
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Header from '../components/header'
 import { getStaticPaths } from './[slug]'
 
 const Home: NextPage = () => {
   return (
-    <div className='my-8 text-center text-4xl font-semibold'>
-      Under Construction
-    </div>
+    <Header />
   )
 }
 
