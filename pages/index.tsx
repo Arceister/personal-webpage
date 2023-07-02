f90da8cadbc02763d930ebeb16ed4d99aab53bb1
Arceister <imandajagad01@gmail.com>
2023-07-02 21:58:25 +0700

    feat: add `main page` component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import Main from '../components/pages/main'
 import { getStaticPaths } from './[slug]'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-black'>
       <Header />
+      <Main />
       <Footer />
     </div>
   )
