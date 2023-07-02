7701656671c2081c5be723451d0d5f5aa388d98a
Arceister <imandajagad01@gmail.com>
2023-07-03 00:39:10 +0700

    fix: fix SEO

--- components/seo.tsx
+++ components/seo.tsx
@@ -3,15 +3,15 @@ import { FC } from "react";
 
 const Seo: FC = () => {
   const meta = {
-    title: 'Jagad\'s Personal Webpage',
-    description: 'A webpage to publish writings or to express opinions about programming/tech stuffs',
+    title: 'Jagad Personal Webpage',
+    description: 'Jagad Nabil Tuah Imanda personal webpage.',
     type: 'website',
     robots: 'follow, index'
   }
 
   return (
     <Head>
-      <title>Jagad&apos;s Personal Webpage</title>
+      <title>Jagad Personal Webpage</title>
       <meta content={meta.description} name="description" />
       <meta name="robots" content={meta.robots} />
       <meta property="og:type" content={meta.type} />
