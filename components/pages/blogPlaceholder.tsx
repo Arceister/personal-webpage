ce8e91676d7e64a228b472f42624c1c6e3c4e716
Arceister <imandajagad01@gmail.com>
2023-08-15 16:41:26 +0700

    fix: fix page template

--- components/pages/blogPlaceholder.tsx
+++ components/pages/blogPlaceholder.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 
 const BlogPlaceholder: FC = () => {
   return (
-    <div className="text-center text-[#1B1C1E] dark:text-[#FEFEFE] py-[20%] lg:py-[17%]">
+    <div className="text-center text-[#1B1C1E] dark:text-[#FEFEFE] py-[20%] mx-[20%] lg:py-[17%]">
       <p className="text-3xl font-semibold">While waiting for this section to be completed, please take a look at my <a href="https://arceister.medium.com/" className="font-bold text-[#0000EE]">Medium</a></p>
     </div>
   )
