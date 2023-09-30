971cd51b4302b0c7146b14937a182a1bcc197fe4
Arceister <imandajagad01@gmail.com>
2023-09-30 20:52:24 +0700

    change main page

--- components/pages/main.tsx
+++ components/pages/main.tsx
@@ -7,7 +7,7 @@ const Main: FC = () => {
         <p className="text-xl sm:text-2xl lg:text-3xl font-semibold pb-[1rem]">Hello there!</p>
         <p className="text-2xl sm:text-4xl lg:text-5xl font-bold"><span className="text-lg sm:text-xl lg:text-2xl pr-[5px] lg:pr-[8px] font-normal">I&apos;m</span>Jagad Nabil Tuah Imanda</p>
       </div>
-      <p className="mt-[10%] lg:mt-[6%] text-xl sm:text-2xl lg:text-3xl text-center">I&apos;m a <span className="font-extrabold">self-proclaimed Software Engineer</span> that can do <span className="font-extrabold">Frontend Development</span> and <span className="font-extrabold">Backend Development</span> well. Through, right now I&apos;m mainly focusing on <span className="font-extrabold">Server-Side</span> and <span className="font-extrabold">Infrastructure Engineering</span> because I work as an <span className="font-extrabold">IT Engineer (API Gateway)</span> at <span className="font-extrabold">IT Group</span>.</p>
+      <p className="mt-[10%] lg:mt-[6%] text-xl sm:text-2xl lg:text-3xl text-center">I&apos;m a <span className="font-bold">Software Engineer</span> that can do <span className="font-bold">Backend Development</span> and <span className="font-bold">Frontend Development</span>. I&apos;m also interested in <span className="font-bold">Server-Side</span> and <span className="font-bold"> Infrastructure Engineering</span> related things. Currently, I&apos;m working as a <span className="font-bold">Software Engineer</span> at <span className="font-bold">Tokopedia</span>.</p>
     </div>
   )
 }
